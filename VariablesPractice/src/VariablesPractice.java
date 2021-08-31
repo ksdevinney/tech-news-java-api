ec827150da29bd1580c17515df8ea06ee6bc95ba
ksdevinney <katie@devinney.org>
2021-08-31 15:15:28 -0500

    practicing with variables

---
+++ VariablesPractice/src/VariablesPractice.java
@@ -0,0 +1,14 @@
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int number = 13;
+        double decimal = 13.13;
+        double sum = number + decimal;
+
+//        System.out.println(sum);
+
+        String word = "gabbaghoul";
+        String adding = word + number;
+
+        System.out.println(adding);
+    }
+}
