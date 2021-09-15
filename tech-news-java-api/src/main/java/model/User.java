9b65aad1d5d07e4bbab6422238f7523ac2e48e97
ksdevinney <katie@devinney.org>
2021-09-15 14:02:57 -0500

    entity models without error in user model

--- tech-news-java-api/src/main/java/model/User.java
+++ tech-news-java-api/src/main/java/model/User.java
@@ -1,6 +1,8 @@
 package model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import model.Post;
+import model.Vote;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -8,30 +10,33 @@ import java.util.List;
 import java.util.Objects;
 
 @Entity
-@JsonIgnoreProperities({"hibernateLazyInitializer", "handler"})
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "user")
-
 public class User implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String username;
-
     @Column(unique = true)
     private String email;
     private String password;
-
     @Transient
     boolean loggedIn;
 
     @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Post> posts;
 
+    // Need to use FetchType.LAZY to resolve multiple bags exception
     @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Vote> votes;
 
+    // Need to use FetchType.LAZY to resolve multiple bags exception
     @OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Comment> comments;
+
+    public User() {
+    }
 
     public User(Integer id, String username, String email, String password) {
         this.id = id;
@@ -39,6 +44,7 @@ public class User implements Serializable {
         this.email = email;
         this.password = password;
     }
+
 
     public Integer getId() {
         return id;
@@ -107,21 +113,21 @@ public class User implements Serializable {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof User)) return false;
         User user = (User) o;
-        return loggedIn == user.loggedIn &&
-                Objects.equals(id, user.id) &&
-                Objects.equals(username, user.username) &&
-                Objects.equals(email, user.email) &&
-                Objects.equals(password, user.password) &&
-                Objects.equals(posts, user.posts) &&
-                Objects.equals(votes, user.votes) &&
-                Objects.equals(comments, user.comments);
+        return isLoggedIn() == user.isLoggedIn() &&
+                Objects.equals(getId(), user.getId()) &&
+                Objects.equals(getUsername(), user.getUsername()) &&
+                Objects.equals(getEmail(), user.getEmail()) &&
+                Objects.equals(getPassword(), user.getPassword()) &&
+                Objects.equals(getPosts(), user.getPosts()) &&
+                Objects.equals(getVotes(), user.getVotes()) &&
+                Objects.equals(getComments(), user.getComments());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, username, email, password, loggedIn, posts, votes, comments);
+        return Objects.hash(getId(), getUsername(), getEmail(), getPassword(), isLoggedIn(), getPosts(), getVotes(), getComments());
     }
 
     @Override
